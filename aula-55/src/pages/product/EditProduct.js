2ee3e9045e2685cce80c92ede772e1702debcfed
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2022-11-29 20:40:09 -0300

    Add lesson 55

---
+++ aula-55/src/pages/product/EditProduct.js
@@ -0,0 +1,65 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { Link, useParams } from 'react-router-dom'
+
+const EditProdut = (props) => {
+    const [preco, setPreco] = useState("")
+    const { id, titulo, descricao } = useParams()
+
+    useEffect(() => {
+        buscarProduto()
+        console.log(props.produto)
+    }, [])
+
+    const buscarProduto = async () => {
+        // const resposta = await axios.get(`http://localhost:3001/produtos/${id}`)
+        // setTitulo(resposta.data.titulo)
+        // setDescricao(resposta.data.descricao)
+        // setPreco(resposta.data.preco)
+     
+    }
+
+    return (
+        <div>
+            <h1>Editar produto</h1>
+            <Link to="/products">
+                <button>
+                    Voltar
+                </button>
+            </Link> 
+            <div>
+                <label>Nome:</label>
+                <input 
+                    type="text" 
+                    name="titulo" 
+                    placeholder="Nome do produto" 
+                    value={titulo}
+                    // onChange={(event) => setTitulo(event.target.value)}
+                 />
+            </div>
+            <div>
+                <label>Descrição:</label>
+                <input 
+                    type="text" 
+                    name="descricao" 
+                    placeholder="Descrição do produto" 
+                    value={descricao} 
+                    // onChange={(event) => setDescricao(event.target.value)}
+                />
+            </div>
+            <div>
+                <label>Preço:</label>
+                <input 
+                    type="text" 
+                    name="preco" 
+                    placeholder="Preço do produto" 
+                    value={preco}
+                    onChange={(event) => setPreco(event.target.value)}
+                />
+            </div>
+            <button>Atualizar</button>
+        </div>
+    )
+}
+
+export default EditProdut
